a8004a7b8aa58b64bb5afdcf58713f27d6d83ad9
Josh <joshfokis@gmail.com>
2022-05-19 00:46:49 -0500

    init push

---
+++ set_generator.py
@@ -0,0 +1,25 @@
+import requests
+
+class SetGenerator():
+
+    def __init__(self):
+        self.response = self.get_set()
+        self.encouters = []
+        self.agendas = []
+        self.acts = []
+        self.locations = []
+        self.title = ''
+        self.game_type = ''
+
+    def parse_encouters(self):
+        pass
+
+    def parse_agendas(self):
+        pass
+
+    def parse_acts(self):
+        pass
+
+    def parse_locations(self):
+        pass
+
